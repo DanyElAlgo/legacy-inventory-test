bcdc8b0a57fcd5eb1b26ed99dcd99e90221bc210
Daniel Roland Peñaranda Colque <eldanielitu@gmail.com>
2026-05-20 20:44:04 -0400

    feature: ips fixed

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "app_sg" {
     from_port   = var.app_port
     to_port     = var.app_port
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["190.104.20.154/32"]
   }
 
   egress {
@@ -15,7 +15,7 @@ resource "aws_security_group" "app_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["190.104.20.154/32"]
   }
 
   tags = {
