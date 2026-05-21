f8a446d44bcd1990e9dd2adfab6c41afe734b249
Daniel Roland Peñaranda Colque <eldanielitu@gmail.com>
2026-05-20 21:02:00 -0400

    fix of the fix

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "app_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["190.104.20.154/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
